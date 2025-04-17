f7559c059a7dbe8973f377931e1164bf9c1473dd
YuvathikaB <yuvathika2020@gmail.com>
2025-04-17 12:00:00 +0530

    Added programs of string builder

---
+++ Day4/StringBuilder/ReverseString.java
@@ -0,0 +1,18 @@
+package Day4.StringBuilder;
+import java.util.*;
+public class ReverseString {
+    public static String reverse(String input) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(input);
+        sb.reverse();
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        String reversed = reverse(input);
+        System.out.println("Reversed string: " + reversed);
+        sc.close();
+    }
+}
